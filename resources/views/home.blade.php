592261c90cbd516c6e1009a665ba6b79d4ffb349
TSCOMjvilla <jvilla@tscom.cl>
2023-09-04 12:21:41 -0300

    buscador y orden

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    @livewire('show-post',['title'=>'este es un titulo de prueba'])
+                    @livewire('show-post')
                 </div>
             </div>
         </div>
--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{$posts}}
+
+    <div class="px6 py-4">
+        <input type="text" wire:model='search' placeholder="buscar...">
+    </div>
+    @if ($posts->count())
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th><button type="button" class="btn" wire:click="order('id')" >ID</button></th>
+                    <th><button type="button" class="btn" wire:click="order('title')">Titulo</button></th>
+                    <th><button type="button" class="btn" wire:click="order('content')">Comentario</button></th>
+                    <th><button type="button" class="btn">Acciones</button></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->content }}</td>
+                        <td>edit</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    @else
+        <div class="px6 py-4">
+            No existe ningun registro coincidente
+        </div>
+    @endif
 </div>
